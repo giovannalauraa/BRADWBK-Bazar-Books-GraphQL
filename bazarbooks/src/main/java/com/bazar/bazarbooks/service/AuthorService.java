93474dcd929c626eee38afbd6a9e32950a685412
Ana Laura <131811398+anaangelieri@users.noreply.github.com>
2025-06-22 23:22:42 -0300

    [ Author ] Implementando

---
+++ bazarbooks/src/main/java/com/bazar/bazarbooks/service/AuthorService.java
@@ -0,0 +1,55 @@
+package br.edu.ifsp.graphql.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.ifsp.graphql.dto.AuthorInput;
+import br.edu.ifsp.graphql.model.Author;
+import br.edu.ifsp.graphql.repository.AuthorRepository;
+
+@Service
+public class AuthorService {
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Author getAuthorById(int id) {
+        return authorRepository.findById(id).orElse(null);
+    }
+
+    public Author createAuthor(AuthorInput authorInput) {
+        Author author = new Author();
+        author.setName(authorInput.getName());
+        author.setDescription(authorInput.getDescription());
+        author.setImageUrl(authorInput.getImageUrl());
+        author.setBooks(authorInput.getBooks());
+        return authorRepository.save(author);
+    }
+
+    public boolean deleteAuthor(int id) {
+        if (authorRepository.existsById(id)) {
+            authorRepository.deleteById(id); 
+            return true;  
+        }
+        return false; 
+    }
+
+    public boolean updateAuthor(int id, AuthorInput authorInput) {
+        if (authorRepository.existsById(id)) {
+            Author author = new Author();
+            author.setIdAuthor(id);  
+            author.setName(authorInput.getName());
+            author.setDescription(authorInput.getDescription());
+            author.setImageUrl(authorInput.getImageUrl());
+            author.setBooks(authorInput.getBooks());
+            authorRepository.save(author);
+            return true;  
+        }
+        return false;  
+    }
+}
