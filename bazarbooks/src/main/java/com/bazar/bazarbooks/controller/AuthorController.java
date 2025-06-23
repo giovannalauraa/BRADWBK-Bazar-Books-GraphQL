d5d69b12b6fdfb8c28978bdddfeb9a8955bb1dfb
Ana Laura <131811398+anaangelieri@users.noreply.github.com>
2025-06-22 23:23:32 -0300

    [ Author ] Implementando

---
+++ bazarbooks/src/main/java/com/bazar/bazarbooks/controller/AuthorController.java
@@ -0,0 +1,51 @@
+package br.edu.ifsp.graphql.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import br.edu.ifsp.graphql.dto.AuthorInput;
+import br.edu.ifsp.graphql.model.Author;
+import br.edu.ifsp.graphql.service.AuthorService;
+
+@Controller
+public class AuthorController {
+
+    @Autowired
+    private AuthorService authorService;
+
+    @QueryMapping
+    public List<Author> getAllAuthors() {
+        return authorService.getAllAuthors();
+    }
+
+    @QueryMapping
+    public Author getAuthorById(@Argument int id) {
+        return authorService.getAuthorById(id);
+    }
+
+    @MutationMapping
+    public Author createAuthor(@Argument AuthorInput authorInput) {
+        Author author = authorService.createAuthor(authorInput);
+        return author;
+    }
+
+    @MutationMapping
+    public Author updateAuthor(@Argument int id, @Argument AuthorInput authorInput) {
+        boolean updated = authorService.updateAuthor(id, authorInput);
+        if (updated) {
+            return authorService.getAuthorById(id);
+        }
+        return null;
+    }
+
+    @MutationMapping
+    public String deleteAuthor(@Argument int id) {
+        boolean deleted = authorService.deleteAuthor(id);
+        return deleted ? "Autor apagado com sucesso!" : "Autor não encontrado.";
+    }
+}
