c5a02e72f7fabeb17fe94a6e4149ebdd5a0cc7c8
Ana Laura <131811398+anaangelieri@users.noreply.github.com>
2025-06-22 23:19:31 -0300

    [ Author ] Implementando

---
+++ bazarbooks/src/main/java/com/bazar/bazarbooks/model/Author.java
@@ -0,0 +1,32 @@
+package br.edu.ifsp.graphql.model;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idAuthor;
+    private String name;
+    private String imageUrl;
+    private String description;
+    @OneToMany(mappedBy = "author")
+    @JsonManagedReference
+    private List<Book> books;
+
+}
