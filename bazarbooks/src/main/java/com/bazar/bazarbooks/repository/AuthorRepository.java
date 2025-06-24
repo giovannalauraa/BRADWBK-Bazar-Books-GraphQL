2808b6b715dcaeda634b0d6fb4e06d62c7965deb
Ana Laura <131811398+anaangelieri@users.noreply.github.com>
2025-06-24 12:28:23 -0300

    [ Author ] Implementando

--- bazarbooks/src/main/java/com/bazar/bazarbooks/repository/AuthorRepository.java
+++ bazarbooks/src/main/java/com/bazar/bazarbooks/repository/AuthorRepository.java
@@ -3,7 +3,7 @@ package com.bazar.bazarbooks.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bazar.bazarbooks.model.Author;
-
+ 
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
     
